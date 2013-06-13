d5a1753abb0f0ded8cad295a37c25cfe0fab5e76
Nando Vieira <fnando.vieira@gmail.com>
2013-06-13 16:19:58 -0300

    Converts XML to UTF-8.

--- lib/aitch/xml_parser.rb
+++ lib/aitch/xml_parser.rb
@@ -1,7 +1,7 @@
 module Aitch
   module XMLParser
     def self.load(source)
-      Nokogiri::XML(source.to_s)
+      Nokogiri::XML(source.to_s, nil, "utf-8")
     end
   end
 end
--- spec/aitch/xml_parser_spec.rb
+++ spec/aitch/xml_parser_spec.rb
@@ -1,11 +1,19 @@
+# -*- encoding: utf-8 -*-
 require "spec_helper"
 
 describe Aitch::XMLParser do
   it "instantiates Nokogiri" do
     Nokogiri
       .should_receive(:XML)
-      .with("XML")
+      .with("XML", nil, "utf-8")
 
     Aitch::XMLParser.load("XML")
+  end
+
+  it "converts ISO-8859-1 to UTF-8" do
+    xml = Aitch::XMLParser.load(File.read("./spec/fixtures/iso8859-1.xml"))
+
+    expect(xml.encoding).to eql("utf-8")
+    expect(xml.to_xml).to include(%[<?xml version="1.0" encoding="utf-8"?>])
   end
 end
